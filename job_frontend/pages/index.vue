61afe652d51037e6335c6b2f534f312a336f2663
matheusgrilo <matheusgrilo@live.com>
2025-01-28 14:40:25 -0300

    Static frontpage

---
+++ job_frontend/pages/index.vue
@@ -0,0 +1,100 @@
+<template>
+  <div>
+    <div class="py-20 px-6 bg-fuchsia-700 text-center">
+      <h1 class="mb-12 text-5xl text-white">Find your new job</h1>
+
+      <NuxtLink
+        to="/browse"
+        class="inline-block py-4 px-8 bg-fuchsia-900 text-white rounded-xl"
+        >Browse jobs</NuxtLink
+      >
+    </div>
+    <div class="py-10 px-6">
+      <h2 class="mb-8 text-2xl text-center">Newest jobs</h2>
+
+      <div class="space-y-4">
+        <div
+          class="p-6 flex items-center justify-between bg-gray-100 dark:bg-gray-800 rounded-xl"
+        >
+          <div>
+            <h3 class="mb-2 text-xl font-semibold text-black dark:text-white">
+              Job position
+            </h3>
+            <p class="text-gray-600 dark:text-gray-300">Company name</p>
+          </div>
+
+          <div>
+            <p class="mb-2">Worldwide</p>
+            <p>U$ 90k~120k</p>
+          </div>
+
+          <div>
+            <p>Posted 2025 Jan. 28</p>
+          </div>
+
+          <div>
+            <NuxtLink
+              to="/browse/1"
+              class="py-4 px-6 bg-fuchsia-700 text-white rounded-xl"
+              >Details</NuxtLink
+            >
+          </div>
+        </div>
+        <div
+          class="p-6 flex items-center justify-between bg-gray-100 dark:bg-gray-800 rounded-xl"
+        >
+          <div>
+            <h3 class="mb-2 text-xl font-semibold text-black dark:text-white">
+              Job position
+            </h3>
+            <p class="text-gray-600 dark:text-gray-300">Company name</p>
+          </div>
+
+          <div>
+            <p class="mb-2">Worldwide</p>
+            <p>U$ 90k~120k</p>
+          </div>
+
+          <div>
+            <p>Posted 2025 Jan. 28</p>
+          </div>
+
+          <div>
+            <NuxtLink
+              to="/browse/1"
+              class="py-4 px-6 bg-fuchsia-700 text-white rounded-xl"
+              >Details</NuxtLink
+            >
+          </div>
+        </div>
+        <div
+          class="p-6 flex items-center justify-between bg-gray-100 dark:bg-gray-800 rounded-xl"
+        >
+          <div>
+            <h3 class="mb-2 text-xl font-semibold text-black dark:text-white">
+              Job position
+            </h3>
+            <p class="text-gray-600 dark:text-gray-300">Company name</p>
+          </div>
+
+          <div>
+            <p class="mb-2">Worldwide</p>
+            <p>U$ 90k~120k</p>
+          </div>
+
+          <div>
+            <p>Posted 2025 Jan. 28</p>
+          </div>
+
+          <div>
+            <NuxtLink
+              to="/browse/1"
+              class="py-4 px-6 bg-fuchsia-700 text-white rounded-xl"
+              >Details</NuxtLink
+            >
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
