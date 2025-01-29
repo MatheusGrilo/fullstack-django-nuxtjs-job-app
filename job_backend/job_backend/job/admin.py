cf4ec23ada4d1536d4edb1976134a726dc8f622e
matheusgrilo <matheusgrilo@live.com>
2025-01-28 23:11:40 -0300

    Add Job and Category models with serializers, views, and API endpoints; update .gitignore

--- job_backend/job_backend/job/admin.py
+++ job_backend/job_backend/job/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Category, Job
+
+admin.site.register(Category)
+admin.site.register(Job)
---
+++ job_backend/job_backend/job/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+
+from .models import Job
+
+
+class JobSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Job
+        fields = (
+            "id",
+            "title",
+            "position_salary",
+            "position_location",
+            "company_name",
+            "company_location",
+            "created_at_formatted",
+        )
